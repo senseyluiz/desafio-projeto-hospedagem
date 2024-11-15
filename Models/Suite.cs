7b6990ec374f12ce31ff1b428169f127b66198dd
Luis Henrique <senseyluiz@gmail.com>
2024-11-15 12:17:42 -0300

    :sparkles: feat(class): Cria classe Suite

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -7,6 +7,68 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
     {
-        
+        public Suite()
+        {
+
+        }
+
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+
+        private string _tipoSuite;
+        private int _capacidade;
+        private decimal _valorDiaria;
+
+        public Guid Id = Guid.NewGuid();
+        public string TipoSuite
+        {
+            get => _tipoSuite;
+            set
+            {
+                if(value =="")
+                {
+                    throw new ArgumentException("O tipo de Suite não pode ser vazio");
+                }
+                _tipoSuite = value;
+            }
+        }
+
+        public int Capacidade
+        {
+            get => _capacidade;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("A capacidade não pode ser menor que 1");
+                }
+                _capacidade = value;
+            }
+        }
+
+        public decimal ValorDiaria
+        {
+            get => _valorDiaria;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("O valor da diária não pode ser menor que 0");
+                }
+                _valorDiaria = value;
+            }
+        }
+
+        public string ResumoSuite()
+        {
+            return $"Id: {Id} \n" +
+                    $"Tipo: {TipoSuite}\n" +
+                    $"Capacidade: {Capacidade}\n" +
+                    $"Diaria: {ValorDiaria.ToString("C")}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@
 
 Pessoa luis = new("Luis Henrique", "Sales da Rocha");
 string nomeCompleto = luis.NomeCompleto();
+Suite suite1 = new("Premium", 3, 30);
 
-System.Console.WriteLine(nomeCompleto);
+
+System.Console.WriteLine(suite1.ResumoSuite());
