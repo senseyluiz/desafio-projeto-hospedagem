fca114b27e41dafe276ad48ec5c183d168d23f2e
Luis Henrique <senseyluiz@gmail.com>
2024-11-15 11:43:27 -0300

    :sparkles: feat(class): Cria classe pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Pessoa
+    {
+
+        public Pessoa()
+        {
+            
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        private string _nome;
+        private string _sobrenome;
+        
+        public Guid Id = Guid.NewGuid();
+        public string Nome
+        {
+            get => _nome;
+            set {
+                if(value ==""){
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+                
+            }
+        }
+        
+        public string Sobrenome
+        {
+            get=> _sobrenome;
+            set {
+                if(value ==""){
+                    throw new ArgumentException("O sobrenome não pode ser vazio");
+                }
+                _sobrenome = value;
+                
+            }
+        }
+        
+
+        public string NomeCompleto()
+        {
+            return $"{Nome} {Sobrenome}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using DesafioProjetoHospedagem.Models;
+
+Pessoa luis = new("Luis Henrique", "Sales da Rocha");
+string nomeCompleto = luis.NomeCompleto();
+
+System.Console.WriteLine(nomeCompleto);
